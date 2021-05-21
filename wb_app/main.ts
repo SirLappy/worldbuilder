c24de88a3051defdefd099589bf46fbcc03e30a0
MikeZ <mike.zaitchik@gmail.com>
2021-05-21 16:08:27 -0700

    updates

--- wb_app/main.ts
+++ wb_app/main.ts
@@ -2,7 +2,7 @@ import { serve } from "./deps.ts";
 
 const PORT = 1993;
 const s = serve(`0.0.0.0:${PORT}`);
-const body = new TextEncoder().encode("Hello Pizza!\n");
+const body = new TextEncoder().encode("Hello Nurse!\n");
 
 console.log(`Server started on port ${PORT}`);
 for await (const req of s) {
